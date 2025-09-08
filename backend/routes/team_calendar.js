a1538ab10aca30a30b25dc22d7ce8eaead6ef723
FrKah <frederic.kah.ing@gmail.com>
2025-09-08 14:10:26 +0200

    Update team calendar query to order slots by date descending

--- backend/routes/team_calendar.js
+++ backend/routes/team_calendar.js
@@ -20,7 +20,7 @@ router.get("/:teamName", async (req, res) => {
       JOIN teams t1 ON g.team1Ref = t1.id
       JOIN teams t2 ON g.team2Ref = t2.id
       WHERE t1.name = ? OR t2.name = ?
-      ORDER BY slots.date ASC
+      ORDER BY slots.date DESC
     `,
       [teamName, teamName]
     );
