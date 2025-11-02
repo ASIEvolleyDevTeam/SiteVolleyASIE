9d51f819d8dac18c317b397964e282a101a89c96
FrKah <frederic.kah.ing@gmail.com>
2025-11-02 12:00:19 +0100

    layout update again

--- frontend/src/components/AddResultForm.tsx
+++ frontend/src/components/AddResultForm.tsx
@@ -82,8 +82,8 @@ export const AddResultForm = () => {
   }
 
   return (
-    <div className="card bg-base-200 items-center p-6">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-6">
+    <div className="card bg-base-200 mt-2 items-center p-6">
+      <form onSubmit={handleSubmit} className="gap-4 p-6">
         <label className="flex flex-col">
           <span className="mb-1 font-semibold">Date</span>
           <input
