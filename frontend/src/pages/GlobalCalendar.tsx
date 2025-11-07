530ba47914468ecccff57f77766c2d6e8dae1137
0rotciv0 <v.halbitte@gmail.com>
2025-11-07 17:57:16 +0100

    trying new commit

--- frontend/src/pages/GlobalCalendar.tsx
+++ frontend/src/pages/GlobalCalendar.tsx
@@ -4,7 +4,7 @@ export const GlobalCalendar = () => {
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="mb-4 pt-8 text-center text-4xl font-bold">
-        Calendrier Global
+        Calendrier Global des matchs
       </h1>
       <GlobalCalendarTable />
     </main>
