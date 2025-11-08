ad06b79565abe7db55334c2751bf2af07d5e77b1
0rotciv0 <v.halbitte@gmail.com>
2025-11-08 17:58:07 +0100

    copy result.js

--- backend/routes/postpone.js
+++ backend/routes/postpone.js
@@ -8,11 +8,19 @@ router.get("/", async (req, res) => {
       SELECT 
         DATE_FORMAT(slots.date, '%d/%m') AS date,
         t1.name AS team1,
-        t2.name AS team2
-      FROM postponed_games p
-      JOIN slots ON p.slotRef = slots.id
-      JOIN teams t1 ON p.team1Ref = t1.id
-      JOIN teams t2 ON p.team2Ref = t2.id
+        t2.name AS team2,
+        g.set1_team1,
+        g.set1_team2,
+        g.set2_team1,
+        g.set2_team2,
+        g.set3_team1,
+        g.set3_team2
+      FROM games g
+      JOIN slots ON g.slotRef = slots.id
+      JOIN teams t1 ON g.team1Ref = t1.id
+      JOIN teams t2 ON g.team2Ref = t2.id
+      WHERE g.set1_team1 IS NOT NULL
+      ORDER BY slots.date ASC
     `);
     res.json(rows);
   } catch (err) {
