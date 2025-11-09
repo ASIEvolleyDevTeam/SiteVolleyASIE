17d9f7149bf47336b55ab8a07602e659b02a2ea2
0rotciv0 <v.halbitte@gmail.com>
2025-11-09 19:07:53 +0100

    changing postpone table

--- backend/routes/postponed_matches.js
+++ backend/routes/postponed_matches.js
@@ -9,12 +9,10 @@ router.get("/", async (req, res) => {
         DATE_FORMAT(slots.date, '%d/%m') AS date,
         t1.name AS team1,
         t2.name AS team2,
-        COALESCE(CONCAT(p.prenom, ' ', p.nom), 'À définir') AS referee
-      FROM games
-      JOIN slots ON games.slotRef = slots.id
-      JOIN teams t1 ON games.team1Ref = t1.id
-      JOIN teams t2 ON games.team2Ref = t2.id
-      WHERE games.team1NoShow = 1 OR games.team2NoShow = 1
+      FROM postponed_games p
+      JOIN slots ON p.slotRef = slots.id
+      JOIN teams t1 ON p.team1Ref = t1.id
+      JOIN teams t2 ON p.team2Ref = t2.id
       ORDER BY slots.date ASC
     `);
     res.json(rows);
